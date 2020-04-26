b0d149a5e782ead76effd711ada049a1e3d80ade
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-04-27 02:31:50 +0530

    Added code to make delete request to server

--- src/scripts/event.js
+++ src/scripts/event.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import config from "./config";
 
 class Event {
-  // this function is used to authenticate user from server
   static create(param) {
     const { eventName, eventSlug, eventDescription } = param;
     return new Promise((resolve, reject) => {
@@ -31,6 +30,27 @@ class Event {
         });
     });
   }
+
+  //   this function is used to delete event
+  static delete(param) {
+    const { slug } = param;
+    return new Promise((resolve, reject) => {
+      axios
+        .delete(`${config.host}/event/${slug}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((resp) => {
+          // save the items in local storage
+          resolve(resp.data);
+        })
+        .catch((error) => {
+          reject(error.response.data);
+        });
+    });
+  }
 }
 
 export const CreateEvent = Event.create;
+export const DeleteEvent = Event.delete;
