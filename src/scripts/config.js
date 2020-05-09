e149aa400105bf2b407c1dcec684356d609edf76
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-05-10 02:00:33 +0530

    Adding remote server

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -2,6 +2,14 @@ const config = {
   host: "https://acm-attendance-backend.azurewebsites.net/",
 };
 
+// const config = {
+//   host: "https://acmvit-attendance-backend.now.sh",
+// };
+
+// const config = {
+//   host: "http://localhost:3000",
+//};
+
 export default config;
 
 export const DateTimeFormat = "dddd, MMMM Do YYYY, h:mm:ss a";
